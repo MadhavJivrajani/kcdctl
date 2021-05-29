bb158250e23fd776aa42feef8608a068166abfb3
Madhav Jivrajani <madhav.jiv@gmail.com>
2021-05-29 15:56:37 +0530

    add cobra boilerplate

---
+++ cmd/root.go
@@ -0,0 +1,69 @@
+/*
+Copyright © 2021 Madhav Jivrajani madhav.jiv@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "kcd-bangalore",
+	Short: "A tool to help demo concepts of imperative and declarative systems as part of the KCD Bangalore Conference",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kcd-bangalore.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".kcd-bangalore" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".kcd-bangalore")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
--- main.go
+++ main.go
@@ -1,51 +1,22 @@
+/*
+Copyright © 2021 Madhav Jivrajani madhav.jiv@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"context"
-	"log"
-
-	"github.com/MadhavJivrajani/kcd-bangalore/pkg/controller"
-	"github.com/MadhavJivrajani/kcd-bangalore/pkg/core"
-	"github.com/MadhavJivrajani/kcd-bangalore/pkg/utils"
-	"github.com/docker/docker/client"
-)
+import "github.com/MadhavJivrajani/kcd-bangalore/cmd"
 
 func main() {
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ctx := context.Background()
-
-	log.Println("Bootstrapping host...")
-	netID, err := utils.BootstrapHost(ctx, cli, core.LoadBalancer{
-		Name:        "lb",
-		ExposedPort: "9090",
-		TargetPort:  "8080",
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Bootstrap successful")
-
-	container := core.Container{
-		Name:          "test",
-		Image:         "nginx",
-		HostPort:      "8080",
-		ContainerPort: "80",
-		Network:       netID,
-	}
-	events := []string{"kill", "stop", "die", "destroy"}
-	desiredState := &core.DesiredState{
-		DesiredNum:    2,
-		ContainerType: container,
-	}
-
-	log.Println("Desired state:", desiredState.DesiredNum)
-	log.Println("Starting controller...")
-	err = controller.Controller(ctx, cli, events, desiredState)
-	if err != nil {
-		log.Fatal(err)
-	}
+	cmd.Execute()
 }
